5890282ba6f3d56c0ae5e2d6f2ec3d6a2dffd2b5
Kyle Goss <kyle@kgwebsites.com>
2018-09-16 00:16:04 -0700

    Started delete functionality

--- system_cleanup.go
+++ system_cleanup.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bufio"
-	"encoding/binary"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"os/user"
 	"path/filepath"
 	"sort"
 	"strings"
@@ -20,7 +22,14 @@ type file struct {
 	path string
 }
 
+type fileID struct {
+	ID   string `json:"ID"`
+	Size int64  `json:"Size"`
+	Path string `json:"Path"`
+}
+
 var files []file
+var selection []fileID
 
 var wg sync.WaitGroup
 var root = "/Users/KyleGoss/Documents/projects"
@@ -38,15 +47,18 @@ func main() {
 
 	// Get the biggest 10 largest files
 	files = files[0:10]
+	for i, f := range files {
+		selection = append(selection, fileID{ID: fmt.Sprint(i + 1), Size: f.size, Path: f.path})
+	}
 
 	// Create tmp dir/files
 	tf, err := createTmp()
-	logErr(err)
+	checkErr(err)
 	defer tf.Close()
 
 	// Write biggest 10 file meta data to tmp file
-	err = writeToTmp(files, tf)
-	logErr(err)
+	err = writeToTmp(selection, tf)
+	checkErr(err)
 
 	// Configure CLI table
 	table := tablewriter.NewWriter(os.Stdout)
@@ -54,10 +66,10 @@ func main() {
 	table.SetRowLine(true)
 
 	// Build CLI table
-	for i, v := range files {
+	for _, v := range selection {
 		// Convert byte size to human readable sizes
-		byteSize := bytefmt.ByteSize(uint64(v.size))
-		table.Append([]string{fmt.Sprint(i + 1), fmt.Sprint(byteSize), trimPath(v.path)})
+		byteSize := bytefmt.ByteSize(uint64(v.Size))
+		table.Append([]string{fmt.Sprint(v.ID), fmt.Sprint(byteSize), trimPath(v.Path)})
 	}
 	table.Render() // Send output
 
@@ -68,7 +80,9 @@ func main() {
 	entry := strings.Split(text, " ")
 	command := entry[0]
 	param := entry[1]
-	fmt.Println(command + " " + param)
+	if command == "delete" {
+		deleteFile(param)
+	}
 	// Delete file by id
 	// Open file by id
 	// Show full path by id
@@ -120,28 +134,48 @@ func createTmp() (*os.File, error) {
 	return tf, nil
 }
 
-func writeToTmp(files []file, tf *os.File) error {
-	for i, f := range files {
-		dataID := make([]byte, 4)
-		binary.LittleEndian.PutUint32(dataID, uint32(i))
-		dataSize := make([]byte, 4)
-		binary.LittleEndian.PutUint32(dataSize, uint32(f.size))
-		dataPath := []byte(f.path)
-		data := append(dataID[:], dataSize[:]...)
-		data = append(data[:], dataPath[:]...)
-
-		// Write file data to temp file
-		_, err := tf.Write(data)
-		if err != nil {
-			return err
-		}
-
-		tf.Sync()
+func writeToTmp(files []fileID, tf *os.File) error {
+	data, err := json.Marshal(files)
+	if err != nil {
+		return err
 	}
+
+	// Write file data to temp file
+	_, err = tf.Write(data)
+	if err != nil {
+		return err
+	}
+
+	tf.Sync()
 	return nil
 }
 
-func logErr(err error) {
+func deleteFile(id string) {
+	var cached []fileID
+	data, err := ioutil.ReadFile(".tmp/cache")
+	checkErr(err)
+	err = json.Unmarshal(data, &cached)
+	checkErr(err)
+	for _, file := range cached {
+		if strings.TrimSpace(file.ID) == strings.TrimSpace(id) {
+			paths := strings.Split(file.Path, "/")
+			fileName := paths[len(paths)-1]
+			// Mac OS
+			// Get user meta data
+			usr, err := user.Current()
+			checkErr(err)
+			// Place the file in the trash
+			err = os.Rename(file.Path, fmt.Sprintf("%v/.Trash/%v", usr.HomeDir, fileName))
+			checkErr(err)
+			// Rewrite the cache
+
+			// Reprint the table
+			fmt.Println(fileName, "moved to the Trash")
+		}
+	}
+}
+
+func checkErr(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
